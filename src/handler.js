bdbc03aa28d3b59d2f718f3ed0f3f31df1a98cb6
hy-reza <93983098+hy-reza@users.noreply.github.com>
2022-06-27 15:56:35 +0700

    Get Notes DONE !

--- src/handler.js
+++ src/handler.js
@@ -27,6 +27,33 @@ export const addNoteHandler = (req, h)  => {
         status: 'fail',
         message: 'Catatan gagal ditambahkan',
       }).code(500);
-
-      
 }
+
+export const getAllNotesHandler = () => ({
+  status : "success",
+  data: {
+    notes,
+  }
+})
+
+export const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+ 
+  const note = notes.filter((n) => n.id === id)[0];
+ 
+ if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+ 
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
-import {addNoteHandler} from './handler.js'
+import {addNoteHandler, getAllNotesHandler, getNoteByIdHandler} from './handler.js'
 
-const routes = [{ 
-    path: '/notes',
-    method: 'POST',
-    handler : addNoteHandler
-
-}, 
-]
+const routes = [
+  {
+    path: "/notes",
+    method: "POST",
+    handler: addNoteHandler,
+  },
+  {
+    path: "/notes",
+    method: "GET",
+    handler: getAllNotesHandler,
+  },
+  {
+    path: "/notes/{id}",
+    method: "GET",
+    handler: getNoteByIdHandler,
+  },
+];
 
 export default routes
